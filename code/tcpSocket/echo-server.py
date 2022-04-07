022bdb7e1db7e3e2f0b231e11109ebb59c709100
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2022-04-07 22:08:15 +0200

    > change logs to be more understandable -> added colors.py

--- code/tcpSocket/echo-server.py
+++ code/tcpSocket/echo-server.py
@@ -2,34 +2,50 @@
 
 import socket
 import json
+import sys
+
 import mysql.connector
 import datetime
+from tabulate import tabulate
+
+from colors import *
 
 HOST = "192.168.178.29"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65435  # Port to listen on (non-privileged ports are > 1023)
 
 connection = mysql.connector.connect(host='192.168.178.107',
                                          database='humidity',
                                          user='test1',
                                          password='test1')
+cursor = connection.cursor()
 
 def insertIntoTable(timestamp, temperature, humidity, tablename):
-    print("Trying to insert into Table",tablename)
+    print(f"{BYellow} -> Trying to insert following data into table",tablename, "\n")
     sql = f"""INSERT INTO {tablename}(timestamp, temperature, humidity) 
                              VALUES 
                              (%s,%s,%s)"""
 
     """ convert seconds into unix timestamp"""
     value = datetime.datetime.fromtimestamp(timestamp)
-    print(value.strftime('%Y-%m-%d %H:%M:%S'))
+    data = [[value, temperature, humidity]]
+    print(f"{Color_Off}",tabulate(data, headers=["timestamp", "temperature", "humidity"]), "\n")
 
     val = (value, temperature, humidity)
 
-    cursor = connection.cursor()
     cursor.execute(sql, val)
     connection.commit()
-    print(cursor.rowcount, "Record inserted successfully into ", tablename)
+    print(f"{BGreen} -> Record inserted successfully into ", tablename)
     cursor.close()
+
+def retrieveData(tablename):
+    print(f"{Color_Off}-> reading table ", tablename)
+    sql = f"""SELECT * FROM {tablename}"""
+    cursor.execute(sql)
+    myresult = cursor.fetchall()
+    print(myresult)
+    """for x in myresult:
+        print(x)
+    cursor.close()"""
 
 def waitingfordata():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -43,26 +59,28 @@ def waitingfordata():
             while True:
                 data = conn.recv(1024)
                 some = json.loads(data.decode("utf-8"))
-                print(f'Retrieved: {data}')
-                print("test",some['timestamp'], some['temperature'], some['humidity'])
                 try:
-                    parsedjson = json.loads(data.decode("utf-8"))
                     insertIntoTable(some['timestamp'], some['temperature'], some['humidity'], "sampleData")
                 except:
-                    #e = sys.exc_info()[0]
-                    print()
+                    e = sys.exc_info()[0]
+                    print(e)
                 finally:
+                    retrieveData("sampleData")
                     s.close()
-                #conn.sendall(json.dumps(data, indent=2).encode('utf-8'))
-
+                #conn.sendall(data)
+                #conn.sendmsg("Test")
+                #s.sendall('Test, Test')
 
 try:
     waitingfordata()
 
 except mysql.connector.Error as error:
-    print(f"Failed to insert record into table ".format(error))
+    print(f"{BRed}","Some Error ".format(error))
 
 finally:
     if connection.is_connected():
         connection.close()
-        print("MySQL connection is closed")
+        print(f"{BRed}","MySQL connection is closed")
+
+
+"""Unused by now"""
