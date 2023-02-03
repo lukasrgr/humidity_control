1060fd9811fffe0a70a5bf24c9f3a758efd6966e
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2023-02-03 23:45:03 +0100

    test ci 6

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -37,8 +37,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
     restartOnFileChange: true
   });
 };
